65375fabc8e9bd8cf9538099ea3d680891f4decb
anooppai <anpspk@gmail.com>
2018-04-04 13:41:10 -0400

    Added Testcase-2 to check Content-Type

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -6,6 +6,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.api.MuleEvent;
+
 
 public class HelloMavenTest extends FunctionalTestCase {
 
@@ -18,6 +20,14 @@ public class HelloMavenTest extends FunctionalTestCase {
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
     
+	@Test
+	 public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+	   System.out.println("\n\nIn Testcase-1: HTTP port used -----------> " + myPort.getNumber());
+	   MuleEvent event = runFlow("retrieveFlights");
+	   String contentType = event.getMessage().getOutboundProperty("Content-Type");
+	   assertEquals("application/json", contentType);
+	 }
+	
 	@Override
     protected String[] getConfigFiles() {
     	String[] files = {"maven-project.xml", "global.xml"};
